30f14ff77aac7387005d4340daa5fa9279dde2b5
sergiomenacho <men19023@byui.edu>
2023-02-13 12:50:54 -0700

    updated

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -6,3 +6,8 @@ if (day == 0 || day == 3 || day == 4 || day == 5 || day == 6) {
 else if (day == 1|| day == 2) {
    document.querySelector(".alert").style.display = "block";
 }
+
+let temp = 33;
+let windspeed = 10;
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+document.querySelector(".wind-chill-number").textContent = `Wind Chill: ${chill}\u00B0`;
